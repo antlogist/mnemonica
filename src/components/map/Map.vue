1f8a9d54786a031b24d3ec9fcd47911ad6146e2e
antlogist <antlogist@gmail.com>
2021-01-03 11:49:22 +0300

    MapBottomNav component created

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <MapField></MapField>
+    <MapBottomNav></MapBottomNav>
   </div>
 </template>
 
 <script>
 import MapField from "@/components/map/map-field/MapField";
+import MapBottomNav from "@/components/map/map-nav/MapBottomNav";
 export default {
   name: "Map",
   methods: {},
   watch: {},
   components: {
-    MapField
+    MapField,
+    MapBottomNav
   }
 };
 </script>
---
+++ src/components/map/map-nav/MapBottomNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-bottom-navigation app>
+    <v-btn @click="newNote">
+      <span>New</span>
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
+    <template v-if="typeof selectedItem == 'number'">
+      <v-btn @click="readNote">
+        <span>Read</span>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn @click="onDeleteNote">
+        <span>Delete</span>
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+    </template>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+ name: "MapBottomNav" 
+}
+</script>
