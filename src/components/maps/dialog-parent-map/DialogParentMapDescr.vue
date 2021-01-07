4247178135d3f34ac9e6b1006a3178842304fc83
antlogist <antlogist@gmail.com>
2021-01-07 22:32:02 +0300

    Map description added

---
+++ src/components/maps/dialog-parent-map/DialogParentMapDescr.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card outlined>
+    <v-edit-dialog persistent :return-value.sync="descr" large>
+      <v-card-text>
+        <div v-html="descr"></div>
+      </v-card-text>
+      <template v-slot:input>
+        <v-textarea v-model="descr" label="Edit" counter></v-textarea>
+      </template>
+    </v-edit-dialog>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "DialogParentMapDescr",
+  data: () => ({}),
+  computed: {
+    ...mapGetters("maps", ["maps", "currentParentMapId"]),
+    descr: {
+      get() {
+        return this.currentParentMapId
+          ? this.maps[this.currentParentMapId]["excerpt"]["descr"]
+          : "";
+      },
+      set(excerpt) {
+        this.maps[this.currentParentMapId]["excerpt"]["descr"] = excerpt;
+      }
+    }
+  }
+};
+</script>
