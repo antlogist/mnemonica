b0f906c7cb15025025b6b2590702c44a14b9c396
antlogist <antlogist@gmail.com>
2021-01-18 23:29:55 +0300

    Child map edit dialog close button added

--- src/components/maps/DialogChildMapEditText.vue
+++ src/components/maps/DialogChildMapEditText.vue
@@ -1,15 +1,20 @@
 <template>
   <v-dialog v-model="isDialogChildMapEditTextShow" fullscreen persistent>
+    <DialogChildMapEditTextToolbar></DialogChildMapEditTextToolbar>
   </v-dialog>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import DialogChildMapEditTextToolbar from "@/components/maps/dialog-child-map-edit-text/DialogChildMapEditTextToolbar";
 export default {
   name: "DialogChildMapEditText",
   data: () => ({}),
   computed: {
     ...mapGetters("maps", ["isDialogChildMapEditTextShow"])
+  },
+  components: {
+    DialogChildMapEditTextToolbar
   }
 };
 </script>
