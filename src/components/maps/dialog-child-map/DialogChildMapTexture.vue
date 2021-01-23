2cdbbe9cf2e88ce8b60b0f377248d95e48726780
antlogist <antlogist@gmail.com>
2021-01-23 23:17:33 +0300

    Background img added

--- src/components/maps/dialog-child-map/DialogChildMapTexture.vue
+++ src/components/maps/dialog-child-map/DialogChildMapTexture.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <v-card outlined>
+    <v-card-text>
+      <v-select :items="textures" v-model="select" label="Texture"></v-select>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: "DialogChildMapTexture"
+  name: "DialogChildMapTexture",
+  data: () => ({
+    textures: ["gray-sand"],
+    address: "../../img/textures/",
+    extension: ".png"
+  }),
+  computed: {
+    ...mapGetters("maps", ["maps", "currentChildMapId", "currentParentMapId"]),
+    select: {
+      get() {
+        const imgAddress = this.currentChildMapId
+          ? this.maps[this.currentParentMapId]["excerpt"]["children"][
+              this.currentChildMapId
+            ]["img"]
+          : "";
+        let img = "";
+        if (imgAddress) {
+          img = imgAddress;
+        }
+        return img;
+      },
+      set(item) {
+        this.maps[this.currentParentMapId]["excerpt"]["children"][
+          this.currentChildMapId
+        ]["img"] = `${this.address}${item}${this.extension}`;
+      }
+    }
+  }
 };
 </script>
