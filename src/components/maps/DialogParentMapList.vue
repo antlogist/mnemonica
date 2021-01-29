18c62db8d10bc4674bb96c468d9190f647cf95a0
antlogist <antlogist@gmail.com>
2021-01-29 14:00:25 +0300

    Dialog parent map list added

---
+++ src/components/maps/DialogParentMapList.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="isDialogParentMapListShow" fullscreen persistent>
+    <v-card>
+      <DialogParentMapListToolbar></DialogParentMapListToolbar>
+      <v-container>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex,
+        impedit sequi, odit voluptates ut voluptate repellat ipsa nostrum
+        dolores adipisci. Dolorum reprehenderit iure est culpa mollitia
+        reiciendis corporis ipsa!
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import DialogParentMapListToolbar from "@/components/maps/dialog-parent-map-list/DialogParentMapListToolbar";
+export default {
+  name: "DialogParentMapList",
+  data: () => ({}),
+  computed: {
+    ...mapGetters("maps", ["isDialogParentMapListShow"])
+  },
+  components: {
+    DialogParentMapListToolbar
+  }
+};
+</script>
+
+<style scoped="true" lang="scss"></style>
---
+++ src/components/maps/dialog-parent-map-list/DialogParentMapListToolbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-toolbar dark color="primary">
+    <template>
+      <v-btn icon dark @click="closeDialogParentMapList">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>Parent Map</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn icon @click="save" dark text>
+          <v-icon>mdi-content-save</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </template>
+  </v-toolbar>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "DialogParentMapToolbar",
+  computed: {},
+  methods: {
+    ...mapActions("maps", ["closeDialogParentMapList"]),
+    closeDialog() {
+      this.closeDialogParentMapList();
+    },
+    save() {
+      console.log("List save");
+    }
+  }
+};
+</script>
